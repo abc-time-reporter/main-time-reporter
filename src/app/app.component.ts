69daa17a45164a29c0b72cbf77fd3ac07031cc67
sakith11 <57568111+sakith11@users.noreply.github.com>
2025-02-17 00:48:15 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent],
+  template: '<app-header />',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'time';
+}
